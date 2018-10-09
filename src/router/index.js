ff87d23f9d5c11e13400e8968d58e97ea5cd2d14
DevrajG <rohannepal200@gmail.com>
2018-10-09 12:25:54 -0400

    chagned UsersTable to Users

--- src/router/index.js
+++ src/router/index.js
@@ -64,7 +64,7 @@ export default new Router({
         {
           name: 'users',
           path: 'users',
-          component: lazyLoading('users/users'),
+          component: lazyLoading('users/Users'),
         },
         {
           name: 'payment',

935fd8031862361c69e0d5ee77e9cccbd0255dc3
DevrajG <rohannepal200@gmail.com>
2018-11-13 14:27:07 -0500

    Refactor unicodechar npm to local

--- src/vuestic-theme/vuestic-components/vuestic-feed/VuesticFeed.vue
+++ src/vuestic-theme/vuestic-components/vuestic-feed/VuesticFeed.vue
@@ -14,89 +14,89 @@
 </template>
 
 <script>
-  import unicodestring from 'unicodechar-string'
+import unicodestring from '../../vuestic-mixins/unicodechar.js'
 
-  export default {
-    name: 'vuestic-feed',
-    props: ['initialPosts'],
-    methods: {
-      removePost (target) {
-        let result = []
-        for (let post of this.posts) {
-          if (post !== target) {
-            result.push(post)
-          }
-        }
-        this.posts = result
-      },
-      changeUnicodeString (str) {
-        return unicodestring(str)
-      }
-    },
-    data () {
-      return {
-        posts: this.initialPosts
-      }
-    }
-  }
+export default {
+	name: "vuestic-feed",
+	props: ["initialPosts"],
+	methods: {
+		removePost(target) {
+			let result = []
+			for (let post of this.posts) {
+				if (post !== target) {
+					result.push(post)
+				}
+			}
+			this.posts = result
+		},
+		changeUnicodeString(str) {
+			return unicodestring(str)
+		}
+	},
+	data() {
+		return {
+			posts: this.initialPosts
+		}
+	}
+}
 </script>
 
 <style lang="scss">
-  .vuestic-feed {
-    padding-top: .5rem;
-    padding-bottom: .5rem;
-    padding-left: 1.5rem;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    .empty {
-      text-align: center;
-    }
-    .post {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      .photo-container {
-        margin-right: 1rem;
-        border-radius: 50%;
-        border: 2px solid $lighter-gray;
-        .photo {
-          background-size: cover !important;
-          width: 3rem;
-          height: 3rem;
-          border-radius: 50%;
-        }
-      }
-      .underscored {
-        width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        border-bottom: $light-gray2 2px solid;
-        overflow: hidden;
-        padding-right: 0.7rem;
-        height: 3.5rem;
-        .text {
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-          .name {
-            font-weight: $font-weight-bold;
-          }
-        }
-        .close-btn {
-          margin-left: 1rem;
-          i {
-            left: 10.5px;
-          }
-        }
-      }
-      &.last {
-        .underscored {
-          border-bottom: none;
-        }
-      }
-    }
-  }
+.vuestic-feed {
+	padding-top: 0.5rem;
+	padding-bottom: 0.5rem;
+	padding-left: 1.5rem;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	.empty {
+		text-align: center;
+	}
+	.post {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		.photo-container {
+			margin-right: 1rem;
+			border-radius: 50%;
+			border: 2px solid $lighter-gray;
+			.photo {
+				background-size: cover !important;
+				width: 3rem;
+				height: 3rem;
+				border-radius: 50%;
+			}
+		}
+		.underscored {
+			width: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			border-bottom: $light-gray2 2px solid;
+			overflow: hidden;
+			padding-right: 0.7rem;
+			height: 3.5rem;
+			.text {
+				overflow: hidden;
+				white-space: nowrap;
+				text-overflow: ellipsis;
+				.name {
+					font-weight: $font-weight-bold;
+				}
+			}
+			.close-btn {
+				margin-left: 1rem;
+				i {
+					left: 10.5px;
+				}
+			}
+		}
+		&.last {
+			.underscored {
+				border-bottom: none;
+			}
+		}
+	}
+}
 </style>
